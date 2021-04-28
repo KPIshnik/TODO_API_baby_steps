f064203a411bccacaa4743eb060f17b9c842582c
KPIshnik <66734177+KPIshnik@users.noreply.github.com>
2021-04-28 19:12:42 +0300

    Delete configs directory
    hide configs

--- configs/connectionConfig.js
+++
@@ -1,7 +0,0 @@
-const connectionConfig = {
-  host: process.env.HOST || "localhost",
-  port: process.env.PORT || 3000,
-};
-
-module.exports.PORT = connectionConfig.port;
-module.exports.HOST = connectionConfig.host;
--- configs/session_config.js
+++
@@ -1,6 +0,0 @@
-const session_config = {
-  secret: process.env.SESSION_KEY || "secret",
-  saveUninitialized: false,
-  resave: true,
-};
-module.exports = session_config;
